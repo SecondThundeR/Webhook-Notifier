cc83b7d8b108b4804ea5b992fbbfdd24facd2da4
SecondThundeR <awayfromgalaxy@gmail.com>
2021-02-05 20:06:20 +0300

    Add check for Discord chars limit

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ with open(FILENAME, 'r', encoding='utf-8') as file:
     CONTENT = file.read()
 file.close()
 
-discord = Discord(url=WEBHOOK_URL)
-discord.post(
-    content=CONTENT,
-    username=WEBHOOK_USERNAME,
-    avatar_url=WEBHOOK_AVATAR_URL
-)
-
-print(f'Message from {FILENAME} was sent successfully')
+if len(CONTENT) - 1 > 2000:
+    print('Sorry, you hit the discord limit of 2000 characters. \n'
+          'Remove some of the content so you can send the message. \n'
+          f'The current number of characters is {len(CONTENT) - 1}')
+else:
+    discord = Discord(url=WEBHOOK_URL)
+    discord.post(
+        content=CONTENT,
+        username=WEBHOOK_USERNAME,
+        avatar_url=WEBHOOK_AVATAR_URL
+    )
+    print(f'Message from {FILENAME} was sent successfully')
